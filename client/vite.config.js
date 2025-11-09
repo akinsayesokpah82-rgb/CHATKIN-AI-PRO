f91a86ccf980c6041b972dd335811ff6562a6e1e
akinsayesokpah82-rgb <akinsayesokpah82@gmail.com>
2025-11-09 10:40:26 +0000

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,3 @@
-// client/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -6,11 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   build: {
-    // ✅ Output directly into server/public
     outDir: path.resolve(__dirname, "../server/public"),
     emptyOutDir: true,
-  },
-  server: {
-    port: 5173,
   },
 });
