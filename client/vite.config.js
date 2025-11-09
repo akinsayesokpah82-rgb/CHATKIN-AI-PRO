247ad6607d61c17448dd7ba73e6d1c7d88c90e47
akinsayesokpah82-rgb <akinsayesokpah82@gmail.com>
2025-11-09 08:24:41 +0000

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname), // make sure Vite starts from client folder
+  root: ".", // this ensures vite looks in client/
   build: {
-    outDir: path.resolve(__dirname, "../server/public"), // build output goes to server/public
+    outDir: "../server/public", // build output goes into server/public
     emptyOutDir: true,
   },
   server: {
